fbrausse/iRRAM

commit 2cec5279083989bedaffa4e0b000ecea960231c5

state access: intro state_proxy being a unique_ptr if TLS is enabled, otherwise leave it as a static object

configure.ac
@@ -108,8 +108,10 @@ AS_IF([test "$tls" = thread_local],[tls_std=1],[tls_std=0])
 AS_IF([test -z "$tls"],
   [AS_CASE([x$enable_tls],
      [xyes],[AC_MSG_ERROR([TLS requested, but could not detect any supported thread-local storage class specifier])],
-     [xdetect],[AC_MSG_WARN([could not detect any supported thread-local storage class specifier; disabling thread support])])],
-  [AC_MSG_NOTICE([using $tls as thread-local storage class specifier])])
+     [xdetect],[AC_MSG_WARN([could not detect any supported thread-local storage class specifier; disabling thread support])])
+   AC_SUBST([iRRAM_HAVE_TLS],[0])],
+  [AC_MSG_NOTICE([using $tls as thread-local storage class specifier])
+   AC_SUBST([iRRAM_HAVE_TLS],[1])])
 AC_SUBST([iRRAM_TLS],[$tls])
 AC_SUBST([iRRAM_TLS_STD],[$tls_std])
 

include/iRRAM/lib.h
@@ -84,40 +84,41 @@ namespace iRRAM {
 template <typename F, typename... Args>
 auto exec(F f, const Args &... args) -> decltype(f(args...))
 {
-	state_t &st = state;
+	state_t &st = *state;
+	ITERATION_DATA &actual_stack = st.ACTUAL_STACK;
 	st.thread_data_address = new iRRAM_thread_data_class;
 
-	stiff code(state.prec_start, stiff::abs{});
+	stiff code(st.prec_start, stiff::abs{});
 	fesetround(FE_DOWNWARD);
 	// set the correct rounding mode for REAL using double intervals):
 
 	st.cache_active = new cachelist;
 
-	if (iRRAM_unlikely(state.debug > 0)) {
+	if (iRRAM_unlikely(st.debug > 0)) {
 //		std::stringstream s;
 //		s << std::this_thread::get_id();
 		cerr << "\niRRAM (version " << iRRAM_VERSION_rt
 		     << ", backend " << iRRAM_BACKENDS << ")"
 //		     << " thread " << s.str()
 		     << " starting...\n";
-		st.max_prec = st.ACTUAL_STACK.prec_step;
+		st.max_prec = actual_stack.prec_step;
 	}
 
 	using RESULT = decltype(f(args...));
 	RESULT result;
 
-	st.ACTUAL_STACK.prec_policy = 1;
-	st.ACTUAL_STACK.inlimit = 0;
-	st.highlevel = (st.ACTUAL_STACK.prec_step > 1);
+	actual_stack.prec_policy = 1;
+	actual_stack.inlimit = 0;
+	st.highlevel = (actual_stack.prec_step > 1);
 
 	while (true) {
 		iRRAM::cout.rewind();
 		for (int n = 0; n < st.max_active; n++)
-			state.cache_active->id[n]->rewind();
+			st.cache_active->id[n]->rewind();
 
 		st.inReiterate = false;
-		assert(st.ACTUAL_STACK.inlimit == 0);
-		assert(st.highlevel == (st.ACTUAL_STACK.prec_step > 1));
+		assert(actual_stack.inlimit == 0);
+		assert(st.highlevel == (actual_stack.prec_step > 1));
 
 		int p_end = 0;
 		try {
@@ -125,30 +126,30 @@ auto exec(F f, const Args &... args) -> decltype(f(args...))
 			if (iRRAM_likely(!st.infinite))
 				break;
 		} catch (const Iteration &it) {
-			p_end = st.ACTUAL_STACK.actual_prec + it.prec_diff;
+			p_end = actual_stack.actual_prec + it.prec_diff;
 		} catch (const iRRAM_Numerical_Exception &exc) {
 			cerr << "iRRAM exception: " << iRRAM_error_msg[exc.type]
 			     << "\n";
 			throw;
 		}
 
-		assert(st.highlevel == (st.ACTUAL_STACK.prec_step > 1));
+		assert(st.highlevel == (actual_stack.prec_step > 1));
 
 		int prec_skip = 0;
 		do {
 			prec_skip++;
 			code.inc_step(4);
-		} while ((st.ACTUAL_STACK.actual_prec > p_end) &&
+		} while ((actual_stack.actual_prec > p_end) &&
 		         (prec_skip != st.prec_skip));
 
-		assert(st.ACTUAL_STACK.inlimit == 0);
+		assert(actual_stack.inlimit == 0);
 		if (iRRAM_unlikely(st.debug > 0)) {
 			show_statistics();
-			if (st.max_prec <= st.ACTUAL_STACK.prec_step)
-				st.max_prec = st.ACTUAL_STACK.prec_step;
+			if (st.max_prec <= actual_stack.prec_step)
+				st.max_prec = actual_stack.prec_step;
 			cerr << "increasing precision bound to "
-			     << st.ACTUAL_STACK.actual_prec << "["
-			     << st.ACTUAL_STACK.prec_step << "]\n";
+			     << actual_stack.actual_prec << "["
+			     << actual_stack.prec_step << "]\n";
 		}
 	}
 

src/REALmain.cc
@@ -92,14 +92,14 @@ void show_statistics()
   cerr << "   CPU-Time for pi:    "<<pi_time<<" s\n";
   cerr << "   total CPU-Time:     "<<time<<" s\n";
   //cerr << "   total Memory:       "<<memory/1024<<" KB\n";
-  if ( state.ACTUAL_STACK.prec_step != 1) 
-    cerr << "   basic precision:    "<<state.ACTUAL_STACK.actual_prec
-		<<"["<<state.ACTUAL_STACK.prec_step<<"]\n"; 
+  if ( actual_stack().prec_step != 1) 
+    cerr << "   basic precision:    "<<actual_stack().actual_prec
+		<<"["<<actual_stack().prec_step<<"]\n"; 
   else 
   cerr << "   basic precision:    double\n"; 
-  if ( state.max_prec != 1) 
-    cerr << "   maximal precision:  "<<iRRAM_prec_array[state.max_prec]
-		<<"["<<state.max_prec<<"]\n"; 
+  if ( state->max_prec != 1) 
+    cerr << "   maximal precision:  "<<iRRAM_prec_array[state->max_prec]
+		<<"["<<state->max_prec<<"]\n"; 
   else 
     cerr << "   maximal precision:  double\n"; 
 }
@@ -126,12 +126,12 @@ extern "C" void iRRAM_initialize2(int *argc, char **argv)
 
 	for (int i = 1; i < *argc; i += 1) {
 		if (!strcmp(argv[i], "-d")) {
-			state.debug = 1;
+			state->debug = 1;
 			iRRAM_DEBUG1(1, "Debugging Mode\n");
 		} else
 		if (!strncmp(argv[i], "--debug=", 8)) {
-			state.debug = atoi(&(argv[i][8]));
-			iRRAM_DEBUG2(1, "Debugging Level %d\n", state.debug);
+			state->debug = atoi(&(argv[i][8]));
+			iRRAM_DEBUG2(1, "Debugging Level %d\n", state->debug);
 		} else
 		if (!strncmp(argv[i], "--prec_init=", 12)) {
 			iRRAM_starting_prec = atoi(&(argv[i][12]));
@@ -160,19 +160,19 @@ extern "C" void iRRAM_initialize2(int *argc, char **argv)
 			int hi;
 			hi = atoi(&(argv[i][12]));
 			if (hi > 0)
-				state.prec_skip = hi;
+				state->prec_skip = hi;
 			iRRAM_DEBUG2(1, "Changed heuristic for precision "
 			                "changes to skip at most %d steps\n",
-			             state.prec_skip);
+			             state->prec_skip);
 		} else
 		if (!strncmp(argv[i], "--prec_start=", 13)) {
 			int hi;
 			hi = atoi(&(argv[i][13]));
 			if (hi > 0)
-				state.prec_start = hi;
+				state->prec_start = hi;
 			iRRAM_DEBUG2(1,
 			             "Changed inital precision step to %d \n",
-			             state.prec_start);
+			             state->prec_start);
 		} else
 		if (!strcmp(argv[i], "-h") || !strcmp(argv[i], "--help")) {
 			fprintf(stderr,
@@ -202,7 +202,7 @@ extern "C" void iRRAM_initialize2(int *argc, char **argv)
 	_iRRAM_prec_array[1] = iRRAM_starting_prec;
 	int prec_inc = iRRAM_prec_inc;
 	double factor = std::sqrt(std::sqrt(iRRAM_prec_factor));
-	if (state.debug)
+	if (state->debug)
 		cerr << "Basic precision bounds: "
 		     << "double[1]";
 	for (int i = 2; i < iRRAM_prec_steps; i++) {
@@ -210,10 +210,10 @@ extern "C" void iRRAM_initialize2(int *argc, char **argv)
 		prec_inc = int(prec_inc * factor) + iRRAM_prec_inc;
 		if (_iRRAM_prec_array[i] >= _iRRAM_prec_array[i - 1])
 			_iRRAM_prec_array[i] = _iRRAM_prec_array[i - 1];
-		else if (state.debug && ((i % 5 == 0) || (i < 10)))
+		else if (state->debug && ((i % 5 == 0) || (i < 10)))
 			cerr << " " << iRRAM_prec_array[i] << "[" << i << "]";
 	}
-	if (state.debug)
+	if (state->debug)
 		cerr << "\n";
 }
 

src/STREAMS.cc
@@ -59,7 +59,7 @@ orstream::orstream()
 }
 orstream::orstream(std::string s, std::ios::openmode mod)
 {
-	if (state.ACTUAL_STACK.inlimit > 0) {
+	if (actual_stack().inlimit > 0) {
 		iRRAM_DEBUG1(2, "I/O-handler: Operation illegal in continuous "
 		                "section!\n");
 		return;
@@ -68,8 +68,8 @@ orstream::orstream(std::string s, std::ios::openmode mod)
 	real_w = 20;
 	real_f = float_form::absolute;
 
-	if (state.ACTUAL_STACK.inlimit == 0) {
-		if (state.thread_data_address->cache_os.get(target)) {
+	if (actual_stack().inlimit == 0) {
+		if (state->thread_data_address->cache_os.get(target)) {
 			/*    state.thread_data_address->cache_ui.get(real_w);
 			    state.thread_data_address->cache_ui.get(real_f);
 			    state.thread_data_address->cache_b.get(_respect_iteration);*/
@@ -81,7 +81,7 @@ orstream::orstream(std::string s, std::ios::openmode mod)
 		target = new std::ofstream(s.c_str(), mod);
 		iRRAM_DEBUG1(2, "I/O-handler: Creating new output stream '"
 		                        << s << "'\n");
-		state.thread_data_address->cache_os.put(target);
+		state->thread_data_address->cache_os.put(target);
 		/*    state.thread_data_address->cache_ui.put(real_w);
 		    state.thread_data_address->cache_ui.put(real_f);
 		    state.thread_data_address->cache_b.put(_respect_iteration);*/
@@ -101,19 +101,19 @@ irstream::irstream()
 
 irstream::irstream(std::string s, std::ios::openmode mod)
 {
-	if (state.ACTUAL_STACK.inlimit > 0) {
+	if (actual_stack().inlimit > 0) {
 		iRRAM_DEBUG1(2, "I/O-handler: Operation illegal in continuous "
 		                "section!\n");
 		return;
 	}
-	if (state.ACTUAL_STACK.inlimit == 0) {
-		if (state.thread_data_address->cache_is.get(target)) {
+	if (actual_stack().inlimit == 0) {
+		if (state->thread_data_address->cache_is.get(target)) {
 			return;
 		}
 		iRRAM_DEBUG1(2, "I/O-handler: Creating new input stream '"
 		                        << s << "'\n");
 		target = new std::ifstream(s.c_str(), mod);
-		state.thread_data_address->cache_is.put(target);
+		state->thread_data_address->cache_is.put(target);
 	} else {
 		iRRAM_DEBUG1(2, "I/O-handler: Creating new input stream '"
 		                        << s << "'\n");
@@ -125,25 +125,25 @@ irstream::irstream(std::string s, std::ios::openmode mod)
 
 
 
-void orstream::rewind() { state.requests = 0; }
+void orstream::rewind() { state->requests = 0; }
 
 void orstream::reset()
 {
-	state.requests = 0;
-	state.outputs = 0;
+	state->requests = 0;
+	state->outputs = 0;
 }
 
 #define iRRAM_outexec(x)                                                       \
-	if (state.inReiterate) {                                               \
+	if (state->inReiterate) {                                              \
 		iRRAM_DEBUG1(2, "I/O-Handler: In iteration, so stream will "   \
 		                "be closed later.\n");                         \
 		return;                                                        \
 	}                                                                      \
-	if (state.ACTUAL_STACK.inlimit > 0) {                                  \
+	if (actual_stack().inlimit > 0) {                                      \
 		iRRAM_DEBUG1(2, "illegal output in continuous section!\n");    \
 		return;                                                        \
 	}                                                                      \
-	if (state.ACTUAL_STACK.inlimit == 0) {                                 \
+	if (actual_stack().inlimit == 0) {                                     \
 		single_valued code;                                            \
 		x;                                                             \
 	} else {                                                               \
@@ -153,15 +153,15 @@ void orstream::reset()
 template <class PARAM>
 static orstream & iRRAM_out(orstream * s, const PARAM & x)
 {
-	if ((state.ACTUAL_STACK.inlimit > 0) && s->_respect_iteration) {
+	if (actual_stack().inlimit > 0 && s->_respect_iteration) {
 		iRRAM_DEBUG1(2, "illegal output in continuous section!\n");
 		return *s;
 	}
-	if ((state.ACTUAL_STACK.inlimit == 0) && s->_respect_iteration) {
+	if (actual_stack().inlimit == 0 && s->_respect_iteration) {
 		single_valued code;
-		if (++state.requests > state.outputs) {
+		if (++state->requests > state->outputs) {
 			*s->target << x;
-			state.outputs++;
+			state->outputs++;
 		}
 	} else {
 		*s->target << x;
@@ -170,7 +170,7 @@ static orstream & iRRAM_out(orstream * s, const PARAM & x)
 }
 
 #define iRRAM_out2(x)                                                          \
-	if ((state.ACTUAL_STACK.inlimit > 0) && _respect_iteration) {          \
+	if (actual_stack().inlimit > 0 && _respect_iteration) {                \
 		iRRAM_DEBUG1(2, "I/O-handler: Illegal output in continuous "   \
 		                "section!\n");                                 \
 		return *this;                                                  \
@@ -210,35 +210,35 @@ orstream & orstream::operator<<(_SetRflags _f) { iRRAM_out2(real_f = _f._M_n); }
 
 orstream::~orstream()
 {
-	if (++state.requests > state.outputs) {
+	if (++state->requests > state->outputs) {
 		if (target != &std::cout && _respect_iteration) {
 			iRRAM_DEBUG1(2, "I/O-handler: Closing handler for "
 			                "output stream\n");
 			iRRAM_outexec(delete target; target = 0;);
 		}
-		state.outputs++;
+		state->outputs++;
 	}
 }
 
 irstream::~irstream()
 {
-	if (++state.requests > state.outputs) {
+	if (++state->requests > state->outputs) {
 		if (target != &std::cin) {
 			iRRAM_DEBUG1(2, "I/O-Handler: Closing handler for "
 			                "input stream\n");
 			iRRAM_outexec(delete target; target = 0;);
 		}
-		state.outputs++;
+		state->outputs++;
 	}
 }
 
 
 #define iRRAM_in(VAR, CACHE)                                                   \
-	if (state.ACTUAL_STACK.inlimit > 0) {                                  \
+	if (actual_stack().inlimit > 0) {                                      \
 		iRRAM_DEBUG1(2, "illegal input in continuous section!\n");     \
 		return *this;                                                  \
 	}                                                                      \
-	if (state.ACTUAL_STACK.inlimit == 0) {                                 \
+	if (actual_stack().inlimit == 0) {                                     \
 		if (CACHE.get(VAR)) {                                          \
 			return *this;                                          \
 		}                                                              \
@@ -250,33 +250,33 @@ irstream::~irstream()
 	return *this;
 
 
-irstream& irstream::operator>>(bool& b)            {iRRAM_in(b,state.thread_data_address->cache_b);}
-irstream& irstream::operator>>(short& i)           {iRRAM_in(i,state.thread_data_address->cache_sh);}
-irstream& irstream::operator>>(unsigned short& ui) {iRRAM_in(ui,state.thread_data_address->cache_ush);}
-irstream& irstream::operator>>(int& i)             {iRRAM_in(i,state.thread_data_address->cache_i);}
-irstream& irstream::operator>>(unsigned int& ui)   {iRRAM_in(ui,state.thread_data_address->cache_ui);}
-irstream& irstream::operator>>(long& l)            {iRRAM_in(l,state.thread_data_address->cache_l);}
-irstream& irstream::operator>>(unsigned long& ul)  {iRRAM_in(ul,state.thread_data_address->cache_ul);}
-irstream& irstream::operator>>(float& d)           {iRRAM_in(d,state.thread_data_address->cache_f);}
-irstream& irstream::operator>>(double& d)          {iRRAM_in(d,state.thread_data_address->cache_d);}
-irstream& irstream::operator>>(long long& ll)      {iRRAM_in(ll,state.thread_data_address->cache_ll);}
-irstream& irstream::operator>>(unsigned long long& ull) {iRRAM_in(ull,state.thread_data_address->cache_ull);}
-irstream& irstream::operator>>(std::string& s)     {iRRAM_in(s,state.thread_data_address->cache_s);}
+irstream& irstream::operator>>(bool& b)            {iRRAM_in(b,state->thread_data_address->cache_b);}
+irstream& irstream::operator>>(short& i)           {iRRAM_in(i,state->thread_data_address->cache_sh);}
+irstream& irstream::operator>>(unsigned short& ui) {iRRAM_in(ui,state->thread_data_address->cache_ush);}
+irstream& irstream::operator>>(int& i)             {iRRAM_in(i,state->thread_data_address->cache_i);}
+irstream& irstream::operator>>(unsigned int& ui)   {iRRAM_in(ui,state->thread_data_address->cache_ui);}
+irstream& irstream::operator>>(long& l)            {iRRAM_in(l,state->thread_data_address->cache_l);}
+irstream& irstream::operator>>(unsigned long& ul)  {iRRAM_in(ul,state->thread_data_address->cache_ul);}
+irstream& irstream::operator>>(float& d)           {iRRAM_in(d,state->thread_data_address->cache_f);}
+irstream& irstream::operator>>(double& d)          {iRRAM_in(d,state->thread_data_address->cache_d);}
+irstream& irstream::operator>>(long long& ll)      {iRRAM_in(ll,state->thread_data_address->cache_ll);}
+irstream& irstream::operator>>(unsigned long long& ull) {iRRAM_in(ull,state->thread_data_address->cache_ull);}
+irstream& irstream::operator>>(std::string& s)     {iRRAM_in(s,state->thread_data_address->cache_s);}
 
 #define iRRAM_in2(VAR, DATA)                                                   \
 	std::string s;                                                         \
-	if (state.ACTUAL_STACK.inlimit > 0) {                                  \
+	if (actual_stack().inlimit > 0) {                                      \
 		iRRAM_DEBUG1(2, "I/O-handler: Illegal input in continuous "    \
 		                "section!\n");                                 \
 		return *this;                                                  \
 	}                                                                      \
-	if (state.ACTUAL_STACK.inlimit == 0) {                                 \
-		if (state.thread_data_address->cache_s.get(s)) {               \
+	if (actual_stack().inlimit == 0) {                                     \
+		if (state->thread_data_address->cache_s.get(s)) {              \
 			VAR = DATA(s);                                         \
 			return *this;                                          \
 		}                                                              \
 		{ single_valued code; *target >> s; VAR = DATA(s); }           \
-		state.thread_data_address->cache_s.put(s);                     \
+		state->thread_data_address->cache_s.put(s);                    \
 	} else {                                                               \
 		*target >> s;                                                  \
 		VAR = DATA(s);                                                 \
@@ -288,12 +288,12 @@ irstream & irstream::operator>>(INTEGER & d) { iRRAM_in2(d, INTEGER); }
 // irstream&  irstream::operator>>(DYADIC& d){iRRAM_in2(d,DYADIC);}
 
 #define iRRAM_inexec(VAR, CACHE, STMNT)                                        \
-	if (state.ACTUAL_STACK.inlimit > 0) {                                  \
+	if (actual_stack().inlimit > 0) {                                      \
 		iRRAM_DEBUG1(2, "I/O-handler: Illegal input in continuous "    \
 		                "section!\n");                                 \
 		return VAR;                                                    \
 	}                                                                      \
-	if (state.ACTUAL_STACK.inlimit == 0) {                                 \
+	if (actual_stack().inlimit == 0) {                                     \
 		if (CACHE.get(VAR)) {                                          \
 			return VAR;                                            \
 		}                                                              \
@@ -307,84 +307,84 @@ irstream & irstream::operator>>(INTEGER & d) { iRRAM_in2(d, INTEGER); }
 bool orstream::eof()
 {
 	bool test = false;
-	iRRAM_inexec(test, state.thread_data_address->cache_b,
+	iRRAM_inexec(test, state->thread_data_address->cache_b,
 	             test = target->eof())
 }
 
 bool orstream::fail()
 {
 	bool test = false;
-	iRRAM_inexec(test, state.thread_data_address->cache_b,
+	iRRAM_inexec(test, state->thread_data_address->cache_b,
 	             test = target->fail())
 }
 
 bool orstream::good()
 {
 	bool test = false;
-	iRRAM_inexec(test, state.thread_data_address->cache_b,
+	iRRAM_inexec(test, state->thread_data_address->cache_b,
 	             test = target->good())
 }
 
 bool orstream::bad()
 {
 	bool test = false;
-	iRRAM_inexec(test, state.thread_data_address->cache_b,
+	iRRAM_inexec(test, state->thread_data_address->cache_b,
 	             test = target->bad())
 }
 
 bool operator!(orstream & x)
 {
 	bool test = false;
-	iRRAM_inexec(test, state.thread_data_address->cache_b,
+	iRRAM_inexec(test, state->thread_data_address->cache_b,
 	             test = (x.target->fail()))
 }
 
 orstream::operator bool()
 {
 	bool test = false;
-	iRRAM_inexec(test, state.thread_data_address->cache_b,
+	iRRAM_inexec(test, state->thread_data_address->cache_b,
 	             test = !(target->fail()))
 }
 
 bool irstream::eof()
 {
 	bool test = false;
-	iRRAM_inexec(test, state.thread_data_address->cache_b,
+	iRRAM_inexec(test, state->thread_data_address->cache_b,
 	             test = target->eof())
 }
 
 bool irstream::fail()
 {
 	bool test = false;
-	iRRAM_inexec(test, state.thread_data_address->cache_b,
+	iRRAM_inexec(test, state->thread_data_address->cache_b,
 	             test = target->fail())
 }
 
 bool irstream::good()
 {
 	bool test = false;
-	iRRAM_inexec(test, state.thread_data_address->cache_b,
+	iRRAM_inexec(test, state->thread_data_address->cache_b,
 	             test = target->good())
 }
 
 bool irstream::bad()
 {
 	bool test = false;
-	iRRAM_inexec(test, state.thread_data_address->cache_b,
+	iRRAM_inexec(test, state->thread_data_address->cache_b,
 	             test = target->bad())
 }
 
 bool operator!(irstream & x)
 {
 	bool test = false;
-	iRRAM_inexec(test, state.thread_data_address->cache_b,
+	iRRAM_inexec(test, state->thread_data_address->cache_b,
 	             test = (x.target->fail()))
 }
 
 irstream::operator bool()
 {
 	bool test = false;
-	iRRAM_inexec(test, state.thread_data_address->cache_b,
+	iRRAM_inexec(test, state->thread_data_address->cache_b,
 	             test = !(target->fail()))
 }
 

commit 98506aed839da1493003809c4d41c4e467e4a024

speedup TLS access throughout (when CXX supports -fno-extern-tls-init) + fix build with -O0.
This is done by consolidating all TLS (for --enable-tls=thread_local) vars in
REALmain translation unit and enabling "-fno-extern-tls-init" by default if
supported. A side effect is the loss of debug output whenever
iRRAM::{cout,clog,cerr,cin} streams are constructed, since this output races
with the construction of iRRAM::state.

Fixing the initialization order places the tls_init call correctly now and thereby
re-allows building with -O0 which was broken since 2cec52790.

configure.ac
@@ -111,7 +111,12 @@ AS_IF([test -z "$tls"],
      [xdetect],[AC_MSG_WARN([could not detect any supported thread-local storage class specifier; disabling thread support])])
    AC_SUBST([iRRAM_HAVE_TLS],[0])],
   [AC_MSG_NOTICE([using $tls as thread-local storage class specifier])
-   AC_SUBST([iRRAM_HAVE_TLS],[1])])
+   AC_SUBST([iRRAM_HAVE_TLS],[1])
+   AC_LANG_PUSH([C++])
+   AX_CHECK_COMPILE_FLAG([-fno-extern-tls-init],
+     [AM_CXXFLAGS="$AM_CXXFLAGS -fno-extern-tls-init"],
+     [AC_MSG_NOTICE([C++ compiler does not support -fno-extern-tls-init -> slower TLS-access])])
+   AC_LANG_POP([C++])])
 AC_SUBST([iRRAM_TLS],[$tls])
 AC_SUBST([iRRAM_TLS_STD],[$tls_std])
 

include/iRRAM/lib.h
@@ -77,6 +77,9 @@ namespace iRRAM {
 // iRRAM_exec template
 
 namespace internal {
+
+void init();
+
 class run {
 
 	state_t &st;
@@ -126,12 +129,14 @@ public:
 template <typename F, typename... Args>
 ret_value_t<F,Args...> exec(F f, const Args &... args)
 {
+	internal::init();
 	return internal::run(*state).exec(f, args...);
 }
 
 template <typename F, typename... Args>
 ret_void_t<F,Args...> exec(F f, const Args &... args)
 {
+	internal::init();
 	internal::run(*state).exec(f, args...);
 }
 

src/REALmain.cc
@@ -32,8 +32,61 @@ MA 02111-1307, USA.
 
 #include <iRRAM/lib.h>
 
+static void init_prec_array(int (&prec_array)[iRRAM_prec_steps],
+                            int starting_prec, int _prec_inc,
+                            double prec_factor, int debug)
+{
+	prec_array[0] = 2100000000;
+	prec_array[1] = starting_prec;
+	double factor = std::sqrt(std::sqrt(prec_factor));
+	int prec_inc = _prec_inc;
+	if (debug)
+		std::cerr << "Basic precision bounds: "
+		     << "double[1]";
+	for (int i = 2; i < iRRAM_prec_steps; i++) {
+		prec_array[i] = starting_prec + prec_inc;
+		prec_inc = int(prec_inc * factor) + _prec_inc;
+		if (prec_array[i] >= prec_array[i - 1])
+			prec_array[i] = prec_array[i - 1];
+		else if (debug && ((i % 5 == 0) || (i < 10)))
+			std::cerr << " " << prec_array[i] << "[" << i << "]";
+	}
+	if (debug)
+		std::cerr << "\n";
+}
 
 namespace iRRAM {
+
+state_proxy<true>::state_proxy()
+: std::unique_ptr<state_t> { std::make_unique<state_t>() }
+{
+}
+
+static struct iRRAM_init_options opts = iRRAM_INIT_OPTIONS_INIT;
+
+iRRAM_TLS state_proxy<iRRAM_HAVE_TLS> state;
+
+iRRAM_TLS orstream cerr(&std::cerr, false);
+iRRAM_TLS orstream clog(&std::clog, false);
+iRRAM_TLS orstream cout;
+iRRAM_TLS irstream cin;
+
+void internal::init()
+{
+	(void)state;
+	(void)cerr;
+	(void)clog;
+	(void)cout;
+	(void)cin;
+
+	state->debug = opts.debug;
+	state->prec_skip = opts.prec_skip;
+	state->prec_start = opts.prec_start;
+
+	MP_initialize;
+	init_prec_array(state->prec_array, opts.starting_prec, opts.prec_inc,
+	                opts.prec_factor, state->debug);
+}
 
 
 /* for debugging (time measuring):*/
@@ -279,42 +332,10 @@ extern "C" int iRRAM_parse_args(struct iRRAM_init_options *opts, int *argc, char
 	return iRRAM_success;
 }
 
-static void init_prec_array(int (&prec_array)[iRRAM_prec_steps],
-                            int starting_prec, int _prec_inc,
-                            double prec_factor)
+extern "C" void iRRAM_initialize3(const struct iRRAM_init_options *_opts)
 {
-	using namespace iRRAM;
-
-	prec_array[0] = 2100000000;
-	prec_array[1] = starting_prec;
-	double factor = std::sqrt(std::sqrt(prec_factor));
-	int prec_inc = _prec_inc;
-	if (state->debug)
-		cerr << "Basic precision bounds: "
-		     << "double[1]";
-	for (int i = 2; i < iRRAM_prec_steps; i++) {
-		prec_array[i] = starting_prec + prec_inc;
-		prec_inc = int(prec_inc * factor) + _prec_inc;
-		if (prec_array[i] >= prec_array[i - 1])
-			prec_array[i] = prec_array[i - 1];
-		else if (state->debug && ((i % 5 == 0) || (i < 10)))
-			cerr << " " << prec_array[i] << "[" << i << "]";
-	}
-	if (state->debug)
-		cerr << "\n";
-}
-
-extern "C" void iRRAM_initialize3(const struct iRRAM_init_options *opts)
-{
-	using namespace iRRAM;
-
-	state->debug = opts->debug;
-	state->prec_skip = opts->prec_skip;
-	state->prec_start = opts->prec_start;
-
-	MP_initialize;
-	init_prec_array(state->prec_array, opts->starting_prec, opts->prec_inc,
-	                opts->prec_factor);
+	iRRAM::opts = *_opts;
+	iRRAM::internal::init();
 }
 
 extern "C" void iRRAM_initialize2(int *argc, char **argv)

src/STREAMS.cc
@@ -36,15 +36,9 @@ MA 02111-1307, USA.
 
 namespace iRRAM {
 
-iRRAM_TLS orstream cerr(&std::cerr, false);
-iRRAM_TLS orstream clog(&std::clog, false);
-iRRAM_TLS orstream cout;
-iRRAM_TLS irstream cin;
-
 orstream::orstream(std::ostream * s, bool respect_iteration)
 {
 	target = s;
-	iRRAM_DEBUG1(2, "I/O-handler: Creating output stream \n");
 	_respect_iteration = respect_iteration;
 	real_w = 20;
 	real_f = float_form::absolute;
@@ -52,7 +46,6 @@ orstream::orstream(std::ostream * s, bool respect_iteration)
 orstream::orstream()
 {
 	target = &std::cout;
-	iRRAM_DEBUG1(2, "I/O-handler: Creating output stream cout\n");
 	_respect_iteration = true;
 	real_w = 20;
 	real_f = float_form::absolute;
@@ -60,8 +53,6 @@ orstream::orstream()
 orstream::orstream(std::string s, std::ios::openmode mod)
 {
 	if (actual_stack().inlimit > 0) {
-		iRRAM_DEBUG1(2, "I/O-handler: Operation illegal in continuous "
-		                "section!\n");
 		return;
 	}
 	_respect_iteration = true;
@@ -72,14 +63,9 @@ orstream::orstream(std::string s, std::ios::openmode mod)
 		/*    get_cached<unsigned int>(real_w);
 		    get_cached<unsigned int>(real_f);
 		    get_cached<bool>(_respect_iteration);*/
-		iRRAM_DEBUG1(2,
-		             "I/O-handler: Recreating output stream '"
-		                     << s << "'(" << real_w << ")\n");
 		return;
 	}
 	target = new std::ofstream(s.c_str(), mod);
-	iRRAM_DEBUG1(2, "I/O-handler: Creating new output stream '"
-	                        << s << "'\n");
 	put_cached(target);
 	/*    put_cached<unsigned int>(real_w);
 	    put_cached<unsigned int>(real_f);
@@ -88,21 +74,16 @@ orstream::orstream(std::string s, std::ios::openmode mod)
 
 irstream::irstream()
 {
-	iRRAM_DEBUG1(2, "I/O-handler: Creating input stream cin\n");
 	target = &std::cin;
 }
 
 irstream::irstream(std::string s, std::ios::openmode mod)
 {
 	if (actual_stack().inlimit > 0) {
-		iRRAM_DEBUG1(2, "I/O-handler: Operation illegal in continuous "
-		                "section!\n");
 		return;
 	}
 	if (get_cached(target))
 		return;
-	iRRAM_DEBUG1(2, "I/O-handler: Creating new input stream '"
-	                        << s << "'\n");
 	target = new std::ifstream(s.c_str(), mod);
 	put_cached(target);
 }
@@ -197,8 +178,6 @@ orstream::~orstream()
 {
 	if (++state->requests > state->outputs) {
 		if (target != &std::cout && _respect_iteration) {
-			iRRAM_DEBUG1(2, "I/O-handler: Closing handler for "
-			                "output stream\n");
 			iRRAM_outexec(delete target; target = 0;);
 		}
 		state->outputs++;
@@ -209,8 +188,6 @@ irstream::~irstream()
 {
 	if (++state->requests > state->outputs) {
 		if (target != &std::cin) {
-			iRRAM_DEBUG1(2, "I/O-Handler: Closing handler for "
-			                "input stream\n");
 			iRRAM_outexec(delete target; target = 0;);
 		}
 		state->outputs++;

src/stack.cc
@@ -43,15 +43,9 @@ namespace iRRAM {
 // iRRAM_TLS bool iRRAM_COMPARE_exact=true; /* unused */
 // iRRAM_TLS int iRRAM_COMPARE_precision=-60; /* unused */
 
-iRRAM_TLS state_proxy<iRRAM_HAVE_TLS> state;
-
 mv_cache::mv_cache() = default;
 mv_cache::~mv_cache() = default;
 
-state_proxy<true>::state_proxy()
-: std::unique_ptr<state_t> { std::make_unique<state_t>() }
-{
-}
-
 } // namespace iRRAM
+
 // 